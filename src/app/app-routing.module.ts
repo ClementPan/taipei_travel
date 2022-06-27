8ab05a543da03e0f136a7540f250bbb2e6a2344c
ClementPan <clementpan2022@gmail.com>
2022-06-27 15:16:33 +0800

    feat: create home and favorite component, setup router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
+import { FavoriteComponent } from './favorite/favorite.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'favorite', component: FavoriteComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
