a20d403b6f66358e560266ed1a2f4d8f6170dfb0
ClementPan <clementpan2022@gmail.com>
2022-06-28 10:06:10 +0800

    feat: create home component, and change page feature

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,6 @@
 import { ApiService } from './../core/services/api.service';
 import { Component, OnInit } from '@angular/core';
-import { map, tap } from 'rxjs/operators';
+import { GetAllResponse, SpotItem } from '../core/interface/interface';
 
 @Component({
   selector: 'app-home',
@@ -8,22 +8,36 @@ import { map, tap } from 'rxjs/operators';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  apiData = ''
+  dataCount = 0
+  spotData: SpotItem[] = []
+  currentPage = 1
+  currentPageCount = 0
 
   constructor(
     private apiService: ApiService
   ) { }
 
   ngOnInit(): void {
-    this.getAll()
+    this.getAll(this.currentPage)
   }
 
-  getAll() {
-    this.apiService.getAll().pipe(
-      map(d => d.data)
-    ).subscribe(r => {
-      console.log('[[[ getAll: ', r);
-      this.apiData = JSON.stringify(r)
+  getAll(pageNum: number): void {
+    this.apiService.getAll(pageNum).subscribe((res: GetAllResponse) => {
+      const { total, data } = res
+      this.dataCount = total
+      this.spotData = data
+      this.currentPageCount = data.length
+      console.log('[[[ data: ', data);
     })
+  }
+
+  getNextData(): void {
+    this.currentPage += 1
+    this.getAll(this.currentPage)
+  }
+
+  getLastData(): void {
+    this.currentPage -= 1
+    this.getAll(this.currentPage)
   }
 }
