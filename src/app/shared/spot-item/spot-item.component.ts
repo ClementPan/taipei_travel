593b965f4182e96242826a54fd915f73c28aa9b5
ClementPan <clementpan2022@gmail.com>
2022-06-28 15:08:20 +0800

    feat: create like/favorite toggle template, style and logics

--- src/app/shared/spot-item/spot-item.component.ts
+++ src/app/shared/spot-item/spot-item.component.ts
@@ -1,5 +1,5 @@
 import { SpotItem } from './../../core/interface/interface';
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-spot-item',
@@ -7,10 +7,26 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./spot-item.component.scss']
 })
 export class SpotItemComponent implements OnInit {
+  @Output() likeToggle = new EventEmitter()
   @Input() item!: SpotItem
+  @Input() liked!: boolean
+  isFavorite: boolean = false
 
   constructor() { }
 
-  ngOnInit(): void { }
+  ngOnInit(): void {
+    this.setOIsFavorite()
+  }
 
+  setOIsFavorite() {
+    this.isFavorite = this.liked
+  }
+
+  toggleFavorite(): void {
+    this.isFavorite = !this.isFavorite
+    this.likeToggle.emit({
+      isFavorite: this.isFavorite,
+      item: this.item
+    })
+  }
 }
